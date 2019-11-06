76f7f3f0a5862294837439e8186e88f4128069c9
Shyam <shyamraeven@gmail.com>
2019-11-06 12:52:36 +0530

    My First Commit

---
+++ QuickStart/src/com/training/Application.java
@@ -0,0 +1,18 @@
+package com.training;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+		List<String> nameList=Arrays.asList("Ramesh","Suresh");
+		nameList.forEach(System.out::println);
+		
+		for(String eachItem: nameList) {
+			System.out.println(nameList);
+		}
+	}
+	
+	
+}
